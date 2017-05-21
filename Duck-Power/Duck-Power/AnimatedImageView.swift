1a1507494d5efc60735a12855212da4926926272
Jeffrey Wu <jeffrey@tophat.com>
2017-05-21 16:56:01 -0400

    Implemented the animated image view

--- Duck-Power/Duck-Power/AnimatedImageView.swift
+++ Duck-Power/Duck-Power/AnimatedImageView.swift
@@ -6,11 +6,37 @@
 //  Copyright © 2017 okAy Studios. All rights reserved.
 //
 
+import UIKit
 import Foundation
 
-class AnimatedImageView: UImageView
+class AnimatedImageView: UIImageView
 {
     var series: [UIImage] = []
+    var interval: Double = 0.5
     
+    fileprivate var currentIndex = 0
     
+    func add(image: UIImage)
+    {
+        series.append(image)
+    }
+    
+    func animate()
+    {
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.repeat], animations: { 
+            self.advanceImage()
+        }, completion: nil)
+    }
+    
+    func stopAnimate()
+    {
+        layer.removeAllAnimations()
+    }
+    
+    func advanceImage()
+    {
+        let newIndex = (currentIndex + 1) % series.count
+        
+        image = series[newIndex]
+    }
 }
