6b02d00fa68506ea52375e6ab9cb8994c99fcdf7
Jeffrey Wu <jeffrey@tophat.com>
2017-05-21 15:19:44 -0400

    First commit

---
+++ Duck-Power/Duck-Power/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Duck-Power
+//
+//  Created by Jeffrey Wu on 2017-05-21.
+//  Copyright © 2017 okAy Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
